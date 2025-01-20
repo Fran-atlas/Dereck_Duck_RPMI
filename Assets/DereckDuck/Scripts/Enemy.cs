2a656db29033cb187c3ed160128980070f54590a
Eletronsky <elenaencinassanchez1@gmail.com>
2025-01-20 13:40:45 +0100

    script enemy

---
+++ Assets/DereckDuck/Scripts/Enemy.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+    [SerializeField] float enemySpeed = -1f;
+    Rigidbody2D rigidbody;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rigidbody = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        rigidbody.velocity = new Vector2(-1, rigidbody.velocity.y);
+    }
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        Debug.Log("Ha salido de la colisión");
+    }
+}
