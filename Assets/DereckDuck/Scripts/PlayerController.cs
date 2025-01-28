db927e62d69768d1c53e025a7aada0f442cebb00
Eletronsky <elenaencinassanchez1@gmail.com>
2025-01-28 13:17:19 +0100

    Animaciones Salto, Correr y Flip Scritp

--- Assets/DereckDuck/Scripts/PlayerController.cs
+++ Assets/DereckDuck/Scripts/PlayerController.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
@@ -11,7 +12,13 @@ public class PlayerController : MonoBehaviour
 
     public float Speed;
     public float jumpForce;
+    private bool isFacingRight = true;
+    [SerializeField] bool isGrounded;
+    [SerializeField] GameObject GroundCheck;
+    [SerializeField] LayerMask groundLayer; //Para almacenar una máscara que tenemos en Unity.
   
+    
+
 
 
     // Start is called before the first frame update
@@ -25,6 +32,7 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        isGrounded = Physics2D.OverlapCircle(GroundCheck.transform.position, 0.1f, groundLayer); //Posición en la que se va a dibujar el círculo.
         Movement();
         Jump();
 
@@ -35,16 +43,47 @@ public class PlayerController : MonoBehaviour
     {
         horizontalInput = Input.GetAxis("Horizontal");
         playerRb.velocity = new Vector2 (horizontalInput * Speed,playerRb.velocity.y);
+        
+                        
+        if (horizontalInput > 0)
+        {
+            anim.SetBool("IsRunning", true);
+            if (!isFacingRight)
+            {
+                Flip();
+            }
+        }
+        if(horizontalInput < 0)
+        {
+            anim.SetBool("IsRunning", true);
+            if (isFacingRight)
+            {
+                Flip();
+            }
+        }
+        if(horizontalInput == 0)
+        {
+            anim.SetBool("IsRunning", false);
+        }
     }
 
     void Jump()
     {
-        if (Input.GetKeyDown(KeyCode.Space))
+        anim.SetBool("IsJumping", !isGrounded);
+        if (Input.GetKeyDown(KeyCode.Space) && isGrounded)
         {
             playerRb.AddForce(Vector3.up * jumpForce, ForceMode2D.Impulse);
         }
+
+
     }
 
-
+    void Flip()
+    {
+        Vector3 currentScale = transform.localScale;
+        currentScale.x *= -1;
+        transform.localScale = currentScale;
+        isFacingRight = !isFacingRight;
+    }
 
 }
